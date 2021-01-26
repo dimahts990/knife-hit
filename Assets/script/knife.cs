79b023289ce5b1263f6e4e3962f90d1204060cb0
dimahts990 <dimahts990@gmail.com>
2021-01-26 14:26:36 +0300

    первая настройка проекта (дабавленны спрайты ножа и дерева)

---
+++ Assets/script/knife.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class knife : MonoBehaviour
+{
+    [SerializeField] Sprite[] skinKnife;
+    [SerializeField] GameObject knifePrefub;
+    public float speed=1;
+    private Vector2 genPos=new Vector2(0,-3.5f);
+    public int activeSkinKnife;
+    private bool hit;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !hit)
+        {
+            hit = true;
+            Instantiate(knifePrefub, genPos, Quaternion.Euler(0, 0, -135));
+
+        }
+        if (hit)
+        {
+            transform.position += new Vector3(0, speed,0)*Time.deltaTime;
+        }
+    }
+    
+}
---
+++ Assets/script/wood.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wood : MonoBehaviour
+{
+    [SerializeField] float speed;
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, 0, speed);
+    }
+}
